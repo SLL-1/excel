17604dc177d262379954092f9f19ef655eea46eb
SLL-1 <1074045510@qq.com>
2020-08-28 21:28:19 +0800

    测试远程push使用

---
+++ src/main/java/com/example/web/StudentController1.java
@@ -0,0 +1,37 @@
+package com.example.web;
+
+import com.example.entity.SelectTemplate;
+import com.example.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class StudentController1 {
+    @Autowired
+    private StudentService studentService;
+
+//    @RequestMapping(value = "/student-list",method=RequestMethod.POST)
+//    private Map<String ,Object> getStudentList( @RequestParam String page,@RequestParam String rows){
+//        Map<String,Object> modelMap=new HashMap<String, Object>();
+//        modelMap.put("student",studentService.getStudentList());
+//        System.out.println(modelMap);
+//        return modelMap;
+//
+//    }
+@RequestMapping(value = "/student-list",method=RequestMethod.POST)
+private Map<String,Object> getStudentList(SelectTemplate selectTemplate){
+//    sort：排序列字段名。
+//    order：排序方式，可以是 'asc' 或者 'desc'，默认值是 'asc'。
+//    page：页码，起始值 1。
+//    rows：每页显示行。
+    Map<String,Object> modelMap=new HashMap<String, Object>();
+    modelMap.put("rows",studentService.getStudentList(selectTemplate));
+    modelMap.put("total",studentService.getStudentTotal(selectTemplate));
+    return modelMap;
+}
+}
